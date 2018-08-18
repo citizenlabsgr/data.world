259210113721bd33812e5eac737ea7689b2f8a9f
James <wilfongjt@gmail.com>
2018-08-18 11:26:07 -0400

    'update raw-data, clean-data, and script files'

--- scripts/README.md
+++ scripts/README.md
@@ -10,24 +10,34 @@
 * Jupyter Notebook installed 
 * Local install of "[Adopt a Drain]"(https://github.com/citizenlabsgr/adopt-a-drain)
 
-## Clone citizenlabsgr/data.world repo
-* Setup Environment Variables
+## Setup Repo
+* First time Setup 
 ```
+### First 
 # clone to local
-git clone https://github.com/citizenlabsgr/data.world/tree/master
+git clone https://github.com/citizenlabsgr/data.world.git
 cd data.world
 # setup Environment variables
-# >> scripts/adopt-a-drain/.env
+### Refresh Repo
+cd data.world
+git pull
+```
+## Setup Environment Variables
+* Open command window in repo folder data.world
+```
+cd data.world/scripts/adopt-a-drain
+GH_USER=your-github-user-name >> .env
+DW_USER=your-data-world-user-name >> .env
+DW_AUTH_TOKEN=dataworld-adm-token >> .env
+```
+## Validate Raw Data
+* Open command window in repo folder data.world
+```
 # start jupyter notebook
 cd scripts/adopt-a-drain
 jupyter-notebook
-
-
-
-
-#
-
 ```
+
 ## Validate Raw Data
 * Test Load Data 
     * open comand window
--- scripts/adopt-a-drain/settings.py
+++ scripts/adopt-a-drain/settings.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 print('settings')
 # settings.py
 
+#  ENV Variable 
+ENV_ERROR = False # set to True when env variables are not set
+
 load_dotenv()
 
 # OR, the same with increased verbosity:
@@ -17,30 +20,42 @@ load_dotenv(dotenv_path=env_path)
 
 # environment variable handler
 from dotenv import load_dotenv
-# junk = os.getenv("junk")
-# print('junk: ', junk, ", environment variable is not set. did you create it in a .env file?")
 
 GH_USER = os.getenv("GH_USER")
 DW_USER = os.getenv("DW_USER")
-# GH_URL = os.getenv("GH_URL")
-print('GH_USER: ', os.getenv("GH_USER"))
-print('DW_USER: ', os.getenv("DW_USER"))
-GH_URL_RAW = "https://raw.githubusercontent.com/%s/data.world/master/raw-data/".replace("%s",os.getenv("GH_USER"))
-GH_URL_CLEAN = "https://raw.githubusercontent.com/%s/data.world/master/clean-data/".replace("%s",os.getenv("GH_USER"))
-# DW_DB_URL = os.getenv("DW_DB_URL")
-DW_DB_URL = "https://api.data.world/v0/datasets/%s/".replace("%s", os.getenv("DW_USER"))
 DW_AUTH_TOKEN = os.getenv("DW_AUTH_TOKEN")
 
-assert GH_USER != None, ", environment variable is not set. did you create it in a .env file?"
-assert DW_USER != None, ", environment variable is not set. did you create it in a .env file?"
-# assert GH_URL != None, ", environment variable is not set. did you create it in a .env file?"
-# assert DW_DB_URL != None, ", environment variable is not set. did you create it in a .env file?"
-assert DW_AUTH_TOKEN != None, ", environment variable is not set. did you create it in a .env file?"
-#assert DW_DB_RW_TOKEN != None, ", environment variable is not set. did you create it in a .env file?"
+# GH_URL = os.getenv("GH_URL")
+# print('GH_USER: ', os.getenv("GH_USER"))
+# print('DW_USER: ', os.getenv("DW_USER"))
+
+if GH_USER == None:
+    ENV_ERROR = True
+    
+if DW_USER == None:
+    ENV_ERROR = True
+
+if DW_AUTH_TOKEN == None:
+    ENV_ERROR = True
+
+#####################
+##### Globals
+#####################
+GH_URL_RAW = None
+GH_URL_CLEAN = None
+DW_DB_URL = None
+
+if not ENV_ERROR:
+    GH_URL_RAW = "https://raw.githubusercontent.com/%s/data.world/master/raw-data/".replace("%s",os.getenv("GH_USER"))
+    GH_URL_CLEAN = "https://raw.githubusercontent.com/%s/data.world/master/clean-data/".replace("%s",os.getenv("GH_USER"))
+    DW_DB_URL = "https://api.data.world/v0/datasets/%s/".replace("%s", os.getenv("DW_USER"))
+
 
 
 def main():
-  assert junk != None
+    assert GH_USER != None, ", Add GH_USER=<your-github-user-name> in a data.world/scripts/adopt-a-world/.env file?"
+    assert DW_USER != None, ", Add DW_USER=<your-github-user-name> in a data.world/scripts/adopt-a-world/.env file?"
+    assert DW_AUTH_TOKEN != None, ", Add DW_AUTH_TOKEN=<your-github-user-name> in a data.world/scripts/adopt-a-world/.env file?"
 
 
 if __name__ == "__main__":
